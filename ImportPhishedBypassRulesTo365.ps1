2d9252fe5368866f0fbe711a256341e4179a590e
fbarker92 <72350921+fbarker92@users.noreply.github.com>
2023-09-07 14:11:37 +0100

    Update ImportPhishedBypassRulesTo365.ps1

--- ImportPhishedBypassRulesTo365.ps1
+++ ImportPhishedBypassRulesTo365.ps1
@@ -1,12 +1,28 @@
-$Uri = 'https://raw.githubusercontent.com/fbarker92/westspring/main/PhishedMailfloWRulesTemplate.xml?token=GHSAT0AAAAAACGIXGKUCSOH24GEDQ7JKHEMZHZPJYQ'
+<# $Uri = 'https://raw.githubusercontent.com/fbarker92/westspring/main/PhishedMailfloWRulesTemplate.xml?token=GHSAT0AAAAAACGIXGKUCSOH24GEDQ7JKHEMZHZPJYQ'
 $PhishedMailflowRule = "C:\TMP\PhishedMailflowRules.xml"
-Invoke-WebRequest -ContentType 'application/xml' -Uri $Uri -OutFile $PhishedMailflowRule
+Invoke-WebRequest -ContentType 'application/xml' -Uri $Uri -OutFile $PhishedMailflowRule #>
 
-# Set variable
+# Set variables
 $365GA = Read-Host "Enter the UPN for a Global Administrator"
 $PhishedIPs = "143.55.236.228,143.55.236.227,143.140.69.192,34.140.69.192,34.22.133.124"
 $PhishedDomains = "psr.phished.io,phished.io"
 $ClientSecurityHeader = Read-Host "Enter the Client Security Header (Found in the phished portal)"
+$xml = @"
+<?xml version="1.0" encoding="utf-16" standalone="yes"?>
+<rules name="TransportVersioned">
+  <rule name="Phished - bypass spam filter" id="f6d71858-c50f-4604-832b-135810ffac03" format="cmdlet">
+    <version requiredMinVersion="15.0.3.0">
+      <commandBlock><![CDATA[New-TransportRule -Name 'Phished - bypass spam filter' -Mode Enforce -HeaderContainsMessageHeader 'X-PHISHTEST' -HeaderContainsWords $ClientSecurityHeader -SetSCL -1 -SetHeaderName '"X-MS-Exchange-Organization-BypassClutter' -SetHeaderValue 'True']]></commandBlock>
+    </version>
+  </rule>
+  <rule name="Phished - bypass junk folder" id="769b1468-05a8-466c-bf42-bc1cf4ad6a84" format="cmdlet">
+    <version requiredMinVersion="15.0.3.0">
+      <commandBlock><![CDATA[New-TransportRule -Name 'Phished - bypass junk folder' -Mode Enforce -HeaderContainsMessageHeader 'X-PHISHTEST' -HeaderContainsWords $ClientSecurityHeader -SetHeaderName 'X-Forefront-Antispam-Report' -SetHeaderValue 'SFV: SKI;']]></commandBlock>
+    </version>
+  </rule>
+</rules>
+"@
+
 
 #Import Exchange Online Management Module
 Import-Module ExchangeOnlineManagement
