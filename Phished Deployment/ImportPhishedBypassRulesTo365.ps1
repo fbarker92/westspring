bbc4d966e63eea899d6ec34696c2c4040222fd2b
fbarker92 <f.barker92@gmail.com>
2023-09-08 15:52:32 +0100

    coom 2

--- Phished Deployment/ImportPhishedBypassRulesTo365.ps1
+++ Phished Deployment/ImportPhishedBypassRulesTo365.ps1
@@ -1,8 +1,8 @@
 # Disable Internet Explorer OOBE
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main" -Name "DisableFirstRunCustomize" -Value 2
 
+# Downloads and saves the Rule XML template
 $Uri = 'https://raw.githubusercontent.com/fbarker92/westspring/main/Phished%20Deployment/PhishedMailfloWRulesTemplate.xml'
-#$PhishedMailflowRule = "C:\TMP\PhishedMailflowRules.xml"
 $xmlDownload = Invoke-WebRequest -ContentType 'application/xml' -Uri $Uri
 
 # Set variables
@@ -20,21 +20,27 @@ Connect-IPPSSession -UserPrincipalName $365GA
 
 # Check if there is a pre-existing PhishSimOverridePolicy to use, will create one if not
 If ((Get-PhishSimOverrideRule).Name.count -ne 0) {
+    Write-Host "A Phising Override Rule already exists ($((Get-PhishSimOverrideRule).Name)), adding the Phished Domains and IPs"
     Get-PhishSimOverrideRule | Set-PhishSimOverrideRule -AddDomains $PhishedDomains -AddSenderIpRanges $PhishedIPs
-} elseif ((Get-PhishSimOverridePolicy).Name.Count -ne 0) {
-    Get-PhishSimOverrideRule | Set-PhishSimOverrideRule -AddDomains $PhishedDomains -AddSenderIpRanges $PhishedIPs
+} elseif (((Get-PhishSimOverridePolicy).Name.Count -ne 0) -and ((Get-PhishSimOverrideRule).Name.Count -eq 0)) {
+    Write-Host "A Phishing Override Policy exists ($((Get-PhishSimOverridePolicy).Name)), but there isn't a rule. Creating the rule 'Phished'"
+    $PolicyName = Get-PhishSimOverridePolicy
+    New-PhishSimOverrideRule -Name 'Phished' -Policy $PolicyName.Name -Domains $PhishedDomains -SenderIpRanges $PhishedIPs
 } else {
-    New-PhishSimOverridePolicy -Name 'PhishedOverrideRule'
+    Write-Host "There is no Policy or Rule, creating a Policy (PhishedOverridePolicy), and Rule (Phished)"
+    New-PhishSimOverridePolicy -Name 'PhishedOverridePolicy'
     New-PhishSimOverrideRule -Name 'Phished' -Policy PhishedOverrideRule -Domains $PhishedDomains -SenderIpRanges $PhishedIPs
 } 
     
-Start-Sleep 5
+Start-Sleep 2
 
 # Connect to the Exchagne Online module
 Connect-ExchangeOnline
 
 # Set the Default Connection Filter policy to Enabled and set the IP ALlow list
+Write-Host "Adding the Phished IP addresses to the Default Connection Filter Policy"
 Set-HostedConnectionFilterPolicy -Identity Default -EnableSafeList $true -IPAllowList $ConnectionFilterPolicyIPs
+Start-Sleep 2
 
 # Import the Mailflow rules
 [xml]$xml = $xmlDownload.Content.tostring().Replace('*ClientSecurityHeader*', $ClientSecurityHeader)
