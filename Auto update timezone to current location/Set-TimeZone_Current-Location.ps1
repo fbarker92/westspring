11efc8d283d2ce0999e5fb6d43f17f12d8a33cbc
fbarker92 <f.barker92@gmail.com>
2023-09-11 12:03:51 +0100

    reee

---
+++ Auto update timezone to current location/Set-TimeZone_Current-Location.ps1
@@ -0,0 +1,18 @@
+$BingKey = "Au6p1Z3k6CYZZ6zgAJnTKa5mCQTIlfQS2imNze6ocEWUJV-vIuepawH8BvHh_ukB"
+$IPInfo = Invoke-RestMethod http://ipinfo.io/json
+$Location = $IPInfo.loc
+
+<# Write-Host “Country : “$IPInfo.country
+Write-Host “Public IP Address : “$IPInfo.ip
+Write-Host “Location : “$IPInfo.loc #>
+
+$BingTimeZoneURL = “http://dev.virtualearth.net/REST/v1/TimeZone/$Location” + “?key=$BingKey”
+
+$ResultTZ = Invoke-RestMethod $BingTimeZoneURL
+
+$WindowsTZ = $ResultTZ.resourceSets.resources.timeZone.windowsTimeZoneId
+
+If (![string]::IsNullOrEmpty($WindowsTZ)){
+Get-TimeZone -Name $WindowsTZ
+Set-TimeZone -Name $WindowsTZ
+}
