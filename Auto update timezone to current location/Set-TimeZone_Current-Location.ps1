85baa2827b98a870d58a919d525ffd30b2c21c41
fbarker92 <f.barker92@gmail.com>
2023-09-14 17:16:02 +0100

    rere

--- Auto update timezone to current location/Set-TimeZone_Current-Location.ps1
+++ Auto update timezone to current location/Set-TimeZone_Current-Location.ps1
@@ -1,9 +1,6 @@
 $BingKey = "Au6p1Z3k6CYZZ6zgAJnTKa5mCQTIlfQS2imNze6ocEWUJV-vIuepawH8BvHh_ukB"
 $IPInfo = Invoke-RestMethod http://ipinfo.io/json
 $Location = $IPInfo.loc
-
-net start w32time
-
 $BingTimeZoneURL = “http://dev.virtualearth.net/REST/v1/TimeZone/$Location” + “?key=$BingKey”
 
 $ResultTZ = Invoke-RestMethod $BingTimeZoneURL
@@ -14,3 +11,7 @@ If (![string]::IsNullOrEmpty($WindowsTZ)){
 Get-TimeZone -Name $WindowsTZ
 Set-TimeZone -Name $WindowsTZ
 }
+
+w32tm /config /update /manualpeerlist:1.uk.pool.ntp.org /syncfromflags:manual /reliable:yes 
+w32tm /resync /rediscover
+Start-Service w32time
