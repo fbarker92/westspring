d0421658467c90859aa0a4021fc94b6cfc19a7ef
fbarker92 <f.barker92@gmail.com>
2023-09-15 17:04:46 +0100

    RERERE

--- SR2 Cloudcall Communicator Update/CC_Comunicator_Update.ps1
+++ SR2 Cloudcall Communicator Update/CC_Comunicator_Update.ps1
@@ -1,1 +1,15 @@
-Invoke-RestMethod -Uri 
+$Uri = "https://cloudcall.com/wp-content/uploads/2022/11/Communicator_6.5.5.2_113417-1.msi_.zip"
+$FilePath =  "C:\IT\Cloudcall_Communicator\"
+$ZIPName = "Communicator.zip"
+$ZIPPath = "$FilePath$ZIPName"
+
+
+If (!(Test-Path $FilePath)) {
+    New-Item -ItemType Directory -Path $FilePath -Force
+}
+
+Invoke-WebRequest -Uri $Uri -OutFile $ZIPPath
+Expand-Archive -Path $ZIPPath -DestinationPath $FilePath 
+$msi = Get-ChildItem -Path $FilePath | Where-Object {$_.Name -like "*.msi"}
+$msi = $msi.Name
+msiexec.exe /i "$FilePath$msi" /qn
