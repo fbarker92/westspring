de0e889c8b3adb16dd10eee4710dc842e3e85876
fbarker92 <f.barker92@gmail.com>
2023-12-19 16:24:42 +0000

    merged

---
+++ Shared Script/Exchange/Mailflow Rules/Director Phishing Rule/Import-DirectorPhishingMailflowRule.ps1
@@ -0,0 +1,30 @@
+# Initialise Variables
+<# $GlobalAdministrator = $null
+$DirectorName = $null
+$AltDirectorEmails = $null #>
+
+# Parameter help description
+Param (
+    [Parameter(Mandatory = $true)][string]$GlobalAdministrator,
+    [Parameter(Mandatory = $true)][string]$DirectorName,
+    [Parameter(Mandatory = $true)][array]$AltDirectorEmails
+)
+
+# Check that the Global admin email is valid
+If ($GlobalAdministrator -notlike "*@*.*") {
+    Write-Host "Please enter a valid global administrator email" -ForegroundColor Red -BackgroundColor White
+    Exit
+}
+ForEach ($AltDirectorEmail in $AltDirectorEmails) {
+    if ($AltDirectorEmail -notlike "*@*.*") {
+        Write-Host "$AltDirectorEmail is not valid, exiting" -ForegroundColor Red -BackgroundColor White
+    }
+}
+<# $AltDirectorEmails = $AltDirectorEmails -join "," #>
+Write-Host "$AltDirectorEmails" -ForegroundColor Green
+
+# Connect to the Exchange online module
+Connect-ExchangeOnline -UserPrincipalName $GlobalAdministrator
+
+# Create the new mailflow rule with the details provided
+New-TransportRule -Name "Director Phishing Rule | $DirectorName" -Mode Enforce -FromScope NotInOrganization -SenderADAttributeContainsWords "DisplayName:$DirectorName" -ExceptIfFrom $AltDirectorEmails -Quarantine $true
