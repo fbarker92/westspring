db50ef7351de0610cd711efe9d9bceae49977b1a
fbarker92 <f.barker92@gmail.com>
2024-01-20 18:53:38 +0000

    updated ya boi

--- Shared Script/Application_Installs-Updates/Install-DotNet_Latest.ps1
+++ Shared Script/Application_Installs-Updates/Install-DotNet_Latest.ps1
@@ -3,7 +3,15 @@ $DirPath =  "C:\IT\.NET_Latest\"
 $InstallerName = "DotNet_SDK_Latest.exe"
 $InstallerPath = "$DirPath$InstallerName"
 $ArgList = '/install /quiet /norestart'
-$start_time = Get-Date
+#$start_time = Get-Date
+$maxRetries = 3
+$retries = 0 
+
+# Validate Uri
+if (-not [uri]::IsWellFormedUriString($Uri, [System.UriKind]::Absolute)) {
+    Write-Error "Invalid URL $Uri"
+    exit 1 
+  }
 
 # Create detination directory if it doesn't already exist
 If (!(Test-Path $DirPath)) {
@@ -12,7 +20,27 @@ If (!(Test-Path $DirPath)) {
 }
 
 # Download the .NET SDK .exe
-Invoke-RestMethod -Uri $Uri -OutFile $InstallerPath
+do {
+    try {
+      Invoke-WebRequest -Uri $Uri -OutFile $InstallerPath
+      break
+    }
+    catch {
+      $retries++
+      if ($retries -ge $maxRetries) {
+        throw "Download failed after $maxRetries retries."
+      }
+      else {
+        Write-Warning "Download failed, retrying..." 
+        Start-Sleep -Seconds 5 
+      }
+    }
+  } while ($true)
+  
+  if (Test-Path $InstallerPath) {
+    Write-Host "Download succeeded."
+  }
+<# Invoke-RestMethod -Uri $Uri -OutFile $InstallerPath
 "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
 Invoke-WebRequest -Uri $Uri -OutFile $InstallerPath;
 If ((Test-Path $InstallerPath)) {
@@ -20,7 +48,7 @@ If ((Test-Path $InstallerPath)) {
 } else {
     Write-Host "the File download failed, please try again" -ForegroundColor Red
     Exit
-}
+} #>
 
 # Install .NET SDK .exe
 Start-Process -FilePath $InstallerPath -ArgumentList $ArgList
