2962b9766dce3a67e476876c37831b1f8ad7e2cc
fbarker92 <f.barker92@gmail.com>
2024-01-22 11:37:57 +0000

    yis

--- Bethany/Get-SharePointSiteAssetURLs.ps1
+++ Bethany/Get-SharePointSiteAssetURLs.ps1
@@ -2,14 +2,13 @@
 Param (
 [string] $SiteURL=$(Read-Host -prompt "Enter the full site URL")
 )
-#$SiteURL= "https://bethanyschooluk.sharepoint.com/sites/HistoryIGCSERJAC23-25"
 $ListName= "Site Assets"
   
 #Connect to PnP Online
 Connect-PnPOnline -Url $SiteURL -Interactive
  
 #Get All Files from the document library - In batches of 500
-$ListItems = Get-PnPListItem -List $ListName -PageSize 500 | Where {$_.FileSystemObjectType -eq "Folder"}
+$ListItems = Get-PnPListItem -List $ListName -PageSize 500 | Where-Object {$_.FileSystemObjectType -eq "Folder"}
   
 #Loop through all documents
 $DocumentsData=@()
