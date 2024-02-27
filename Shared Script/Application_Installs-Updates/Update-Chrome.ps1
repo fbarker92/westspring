c66498e72174426fcf6315c6aed476288480cc55
fbarker92 <f.barker92@gmail.com>
2024-02-27 09:29:03 +0000

    coomy

--- Shared Script/Application_Installs-Updates/Update-Chrome.ps1
+++ Shared Script/Application_Installs-Updates/Update-Chrome.ps1
@@ -1,23 +1,33 @@
 $Uri = "http://dl.google.com/edgedl/chrome/install/GoogleChromeStandaloneEnterprise64.msi"
-$FilePath =  "C:\IT\Google_Chrome\"
-$MSIName = "google-chrome.msi"
-$MSIPath = "$FilePath$MSIName"
+$OutPath =  "C:\IT\Google_Chrome\"
+$OutName = "google-chrome.msi"
+$OutFile = "$OutPath$OutName"
+$isLocked = $true
 
 # Create detination directory if it doesn't already exist
-If (!(Test-Path $FilePath)) {
-    New-Item -ItemType Directory -Path $FilePath -Force
+If (!(Test-Path $OutPath)) {
+    New-Item -ItemType Directory -Path $OutPath -Force
 }
 
 # Download the latest Chrome .msi
-Invoke-WebRequest -Uri $Uri -OutFile $MSIPath
-Do {
-    Start-Sleep -Seconds 5
-    $downloadStatus = Get-Item $MSIPath
-} While ($downloadStatus.Length -lt $downloadStatus.Length)
+Invoke-WebRequest -Uri $Uri -OutFile $OutFile
+
+while ($isLocked) {
+    Try {
+        $stream = [System.IO.File]::Open($OutFile, 'Open', 'Write')
+        $stream.Close()
+        $stream.Dispose()
+        $isLocked = $false
+    }
+    catch {
+        Start-Sleep -Seconds 30
+    }
+}
 
 
 # Install Chrome .msi
-msiexec.exe /i $MSIPath /qn /norestart
+msiexec.exe /i $OutFile /qn /norestart
 
+Start-Sleep -Seconds 300
 # Clean up
-Remove-Item -Path $FilePath -Recurse -ErrorAction SilentlyContinue
+Remove-Item -Path $OutFile -Recurse -ErrorAction SilentlyContinue
