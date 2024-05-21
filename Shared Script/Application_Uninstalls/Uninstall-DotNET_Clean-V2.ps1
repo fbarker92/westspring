ce2f05c0f94926fd8a0b5a6d2fa64d61e973a612
fbarker92 <f.barker92@gmail.com>
2024-05-21 07:55:31 +0100

    co

--- Shared Script/Application_Uninstalls/Uninstall-DotNET_Clean-V2.ps1
+++ Shared Script/Application_Uninstalls/Uninstall-DotNET_Clean-V2.ps1
@@ -1,22 +1,13 @@
 # Remove all donet componants
 Set-Location "C:\Program Files (x86)\dotnet-core-uninstall\"
-$Version = "{[.NET_Version_Number]}"
-$Arguments = @("remove --all --aspnet-runtime  --force --yes", 
-            "remove --all --hosting-bundle  --force --yes", 
-            "remove --all --runtime  --force --yes", 
-            "remove --all --sdk  --force --yes")
+$BaseDir = "C:\Program Files\dotnet\shared"
+$Vers = "4.6.2"
+$Args = @("remove --all-below $Vers --aspnet-runtime --force --yes", 
+               "remove --all-below $Vers --hosting-bundle --force --yes", 
+               "remove --all-below $Vers --runtime --force --yes", 
+               "remove --all-below $Vers --sdk --force --yes")
 
-foreach ($Argument in $Arguments) {
-    Write-Host $Argument
-    Write-Host "Now uninstalling  "$Argument.Substring(13)""
-    #Start-Process dotnet-core-uninstall.exe $Argument
+foreach ($Arg in $Args) {
+    Write-Host "Now uninstalling"$Arg.Substring(13)""
+    Start-Process dotnet-core-uninstall.exe $Argument
 }
-
-foreach ($version in $removeSDKs) {
-    if (Test-Path $installPath) {
-        Write-Host "Removing .NET SDK $version at C:\Program Files\dotnet\sdk\$version" 
-        Remove-Item $installPath -Recurse -Force -ErrorAction SilentlyContinue
-      }
-}
-
-C:\Program Files\dotnet\shared\Microsoft.NETCore.App\
