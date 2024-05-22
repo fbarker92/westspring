0f33768de80f0e9a3fc191ed711a5c148bb9bd5b
fbarker92 <f.barker92@gmail.com>
2024-05-22 20:31:56 +0100

    mmmmm

--- Shared Script/Application_Installs-Updates/Update-Curl_Latest.ps1
+++ Shared Script/Application_Installs-Updates/Update-Curl_Latest.ps1
@@ -2,6 +2,10 @@ $uri = "https://curl.se/windows/latest.cgi?p=win64-mingw.zip"
 $BaseDir = "C:\Windows\System32"
 $OutFile = "$env:TEMP\curl-latest.zip"
 $CurrVers = (Get-Item -Path "$BaseDir\curl.exe").VersionInfo.ProductVersion
+$Acl = Get-Acl -Path $BaseDir\curl.exe
+$Account = New-Object -TypeName System.Security.Principal.NTAccount -ArgumentList 'BUILTIN\Administrators';
+$TmpAcl = $Acl.SetOwner($Account)
+
 
 Invoke-WebRequest -Uri $uri -OutFile $OutFile
 If (Test-Path -Path $OutFile) {
@@ -18,12 +22,14 @@ If ($LatestCurl.VersionInfo.ProductVersion -le $CurrVers) {
     Exit 1
 } else {
     Write-Host "Latest curl version is greater than current version. Updating..."
+    Set-Acl -Path $BaseDir\curl.exe -AclObject $TmpAcl
     Rename-Item -Path "$BaseDir\curl.exe" -NewName "curl-$CurrVers.exe" -Force
     Write-Host "Renaming curl.exe to curl-$CurrVers.exe"
     Write-Host "Copying curl.exe : $($LatestCurl.VersionInfo.ProductVersion) to $BaseDir "
     Copy-Item -Path $LatestCurl.FullName -Destination "$BaseDir\curl.exe" -Force
     $NewVers = (Get-Item -Path "$BaseDir\curl.exe").VersionInfo.ProductVersion
     Write-Host "Curl has been updated from $CurrVers to $NewVers"
+    Set-Acl -Path $BaseDir\curl.exe -AclObject $Acl
 }
 
 Write-Host "Removing Temp Files..."
