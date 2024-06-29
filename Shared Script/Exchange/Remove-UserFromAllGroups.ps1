b39a249a65fa9f02d92b46fe264389a011f57653
fbarker92 <f.barker92@gmail.com>
2024-06-29 08:47:45 +0100

    mm

--- Shared Script/Exchange/Remove-UserFromAllGroups.ps1
+++ Shared Script/Exchange/Remove-UserFromAllGroups.ps1
@@ -19,12 +19,19 @@ $EXOUnifiedGroups = Get-UnifiedGroup -ResultSize Unlimited
 $EXODLGroups = Get-DistributionGroup -ResultSize Unlimited
 
 ForEach ($EXOUnifiedGroup in $EXOUnifiedGroups) {
-    Get-UnifiedGroupLinks -Identity $EXOUnifiedGroup.Identity -LinkType Members #| Where-Object {$_.Name -eq $User} | Select-Object PrimarySmtpAddress
+    Get-UnifiedGroupLinks -Identity $EXOUnifiedGroup.Identity -LinkType Members | Where-Object {$_.PrimarySmtpAddress -eq $User}
+    [PSCustomObject]@{
+        DisplayName = $EXOUnifiedGroup.DisplayName
+        Id = $groEXOUnifiedGroupup.Id
+        MailEnabled = $true
+        Mail = $EXOUnifiedGroup.PrimarySmtpAddress
+             } #| Export-Csv -Path .\$User-$Date-Groups.csv -Append -NoTypeInformation
 }
 
 foreach ($group in $MGmemberOf) {
        If ($null -eq (Get-MgGroup -GroupId $group.Id).OnpremisesDomainName){
         Write-Host "Removing $User from group: " $(Get-MgGroup -GroupId $group.Id).DisplayName
+        Remove-MgGroupMember -GroupId $group.Id -MemberId $User
     } else {
         Write-Host "Group "($(Get-MgGroup -GroupId $group.Id).DisplayName)" is managed on prem, please remove user manually from the $((Get-MgGroup -GroupId $group.Id).OnpremisesDomainName) Domain"
     }
@@ -33,5 +40,5 @@ foreach ($group in $MGmemberOf) {
         Id = $group.Id
         MailEnabled = $(Get-MgGroup -GroupId $group.Id).MailEnabled
         Mail = $(Get-MgGroup -GroupId $group.Id).Mail
-             } | Export-Csv -Path C:\$User-$Date-Groups.csv -Append -NoTypeInformation
+             } #| Export-Csv -Path .\$User-$Date-Groups.csv -Append -NoTypeInformation
 }
